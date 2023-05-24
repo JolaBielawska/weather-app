52ff9088591accee10dd334ff092b89ad80a39ad
JolaBielawska <130394173+JolaBielawska@users.noreply.github.com>
2023-05-23 21:07:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+//challenge 1//
+
+function formatdate(date) {
+
+let hours=date.getHours();
+if (hours<10){
+hours=`0${hours}`;
+}
+
+let minutes=date.getMinutes();
+if (minutes<10){
+  minutes=`0${minutes}`;
+}
+
+let dayIndex=date.getDay();
+
+let days= [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let day=days[dayIndex];
+return `${day}, ${hours}:${minutes}`;
+}
+
+let dateElement=document.querySelector("#date");
+let currentTime=new Date();
+dateElement.innerHTML=formatdate(currentTime);
+
+//challenge 2//
+
+function displayWeatherCondition(response) {
+  console.log(response.data.name);
+  document.querySelector("#city").innerHTML=response.data.name;
+  document.querySelector("#temperature").innerHTML=Math.round(response.data.main.temp);
+  document.querySelector("#humidity").innerHTML=response.data.main.humidity;
+  document.querySelector("#wind").innerHTML=Math.round(response.data.wind.speed);
+  document.querySelector("#description").innerHTML=response.data.weather[0].main;
+}
+
+function searchCity(city) {
+    let apiKey = "64469ac67e6dc941feb5b50915a18dc7";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+ let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+//test//
+
+function searchLocation(position) {
+  let apiKey = "64469ac67e6dc941feb5b50915a18dc7";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentPosition);
+
+//test//
+
+
+  
+let weathersearchform = document.querySelector("#search-form");
+weathersearchform.addEventListener("submit", handleSubmit);
+
+
+
+searchCity("Charlotte");
+
+
+//challenge 3//
+
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = "19";
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = "66";
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheitlink");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsiuslink");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+
+
+
